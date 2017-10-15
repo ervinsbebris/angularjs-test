fc4dcf6dda652aeb867cdbe2ad2a2370982de50e
Ervins Bebris <ervins.bebris@visma.com>
2017-10-15 20:02:21 +0300

    module 5 missing files

---
+++ AngularWebApp/wwwroot/module5-solution/src/public/newsletter/myinfo.controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+	"use strict";
+
+	angular.module('public')
+		.controller('MyinfoController', MyinfoController);
+
+	MyinfoController.$inject = ['$http', 'ApiPath', 'SignupService'];
+	function MyinfoController($http, ApiPath, SignupService) {
+		var $ctrl = this;
+		$ctrl.userData = SignupService.userData;
+		$ctrl.saved = SignupService.userData.menuNumber != null;
+	}
+})();
---
+++ AngularWebApp/wwwroot/module5-solution/src/public/newsletter/signup.controller.js
@@ -0,0 +1,36 @@
+﻿(function () {
+	"use strict";
+
+	angular.module('public')
+		.controller('SignupController', SignupController);
+
+	SignupController.$inject = ['$http', 'ApiPath', 'SignupService'];
+	function SignupController($http, ApiPath, SignupService) {
+		var $ctrl = this;
+		$ctrl.userData = SignupService.userData;
+		$ctrl.saved = SignupService.userData.menuNumber != null;
+		$ctrl.menuNumberDoesNotExist = false;
+
+		$ctrl.signup = function () {
+
+			var promise = SignupService.CheckMenuNumber($ctrl.userData.menuNumber);
+
+			promise.then(function (response) {
+				$ctrl.menuNumberDoesNotExist = false;
+				$ctrl.userData.menuItem = response;
+				SignupService.SaveInfo($ctrl.userData);
+				$ctrl.saved = true;
+			},
+			function (error) {
+				$ctrl.menuNumberDoesNotExist = true;
+				$ctrl.saved = false;
+			})
+			.catch(function (err) {
+				$ctrl.menuNumberDoesNotExist = true;
+				$ctrl.saved = false;
+			});
+		};
+	}
+
+
+})();
